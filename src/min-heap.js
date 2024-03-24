da09c499edd52399f4075ae08dce06a64eec0ca1
yeyu98 <763525908@qq.com>
2024-03-24 22:58:28 +0800

    fix: 测试

--- src/min-heap.js
+++ src/min-heap.js
@@ -2,7 +2,7 @@
  * @Author: yeyu98
  * @Date: 2024-03-23 14:58:05
  * @LastEditors: yeyu98
- * @LastEditTime: 2024-03-24 22:44:25
+ * @LastEditTime: 2024-03-24 22:52:51
  * @Description: 
  */
 // 数组实现 [0，4， 1， 10， 8， 6, 5]
@@ -10,7 +10,8 @@
 class MinHeap {
     heap = []
     shiftDown(node, index) {
-        while(true) {
+        const length = this.heap.length
+        while(index < length) {
             const leftIndex = (index + 1) * 2 - 1
             const leftNode = this.heap[leftIndex]
             const rightIndex = (index + 1) * 2
@@ -43,7 +44,7 @@ class MinHeap {
         while(true) {
             const parentIndex = (index - 1) >>> 1
             const parent = this.heap[parentIndex]
-            if(parent && parent > node) {
+            if(parent !== undefined && parent > node) {
                 this.heap[index] = this.heap[parentIndex]
                 this.heap[parentIndex] = node 
                 index = parentIndex
@@ -53,16 +54,18 @@ class MinHeap {
         }
     }
     push(node) {
+        console.log(node)
         // 入堆：入堆会从数组最后一个位置进入，不断的与其父级节点比较若父级节点大于则交换两者的位置
         // 需要计算父节点位置 Math.floor(index - 1 / 2) === index - 1 >>> 1
         const first = this.peek()
-        if(!first) {
+        if(first === undefined) {
             this.heap.push(node)
             return
         }
         this.heap.push(node)
         const currentIndex = this.heap.length - 1 // 当前位置
         this.shiftUp(node, currentIndex)
+        console.log('this.heap', this.heap)
     }
     pop() {
         // 出堆：出堆之后需要把最后一个节点移到堆顶，之后至上而下的与每个左右子节点比较大小，与最小的节点交换二者位置直到叶子节点
@@ -70,7 +73,7 @@ class MinHeap {
         // 没有子节点时
         // 左节点位置 (index + 1) * 2 - 1  右节点位置 (index + 1) * 2
         const first = this.heap[0]
-        if(!first) {
+        if(first === undefined) {
             return
         }
         if(this.heap.length === 1) {
@@ -79,6 +82,7 @@ class MinHeap {
         }
         const last = this.heap.pop()
         this.shiftDown(last, 0)
+        console.log('pop', this.heap)
         return first
     }
     peek() {
@@ -86,4 +90,22 @@ class MinHeap {
         return fist ? fist : null
     }
 }
+
+const heap = new MinHeap()
+heap.push(5)
+heap.push(1)
+heap.push(2)
+heap.push(0)
+heap.push(10)
+heap.push(11)
+console.log('heap.pop()', heap.pop())
+console.log('heap.pop()', heap.pop())
+heap.push(6)
+heap.push(8)
+console.log('heap.pop()', heap.pop())
+console.log('heap.pop()', heap.pop())
+console.log('heap.pop()', heap.pop())
+console.log('heap.pop()', heap.pop())
+console.log('heap.pop()', heap.pop())
+
 
